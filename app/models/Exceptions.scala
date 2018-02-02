8359de4d09660bf98ba2b2efc4559fe973501e91
urquha <andrew.urquhart@ons.gov.uk>
2018-02-02 11:52:43 +0000

    Changed exception name from MyException to ServerErrorException

--- app/models/Exceptions.scala
+++ app/models/Exceptions.scala
@@ -4,9 +4,9 @@ package models
  * Created by coolit on 16/11/2017.
  * https://stackoverflow.com/questions/38243530/custom-exception-in-scala
  */
-sealed trait MyException {
+sealed trait ServerErrorException {
   self: Throwable =>
   val message: String
 }
 
-case class ServerError(message: String) extends Exception(message) with MyException
+case class ServerError(message: String) extends Exception(message) with ServerErrorException
