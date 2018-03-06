c66b83855f6de75838dcb353df9e5d0cde9193ea
urquha <andrew.urquhart@ons.gov.uk>
2018-03-06 15:18:43 +0000

    Added working HBase rest unit test
    Changed application.test.conf
    initialize = true ->> initialize = false
    (may break other tests)

--- test/controllers/v1/TestUtils.scala
+++ test/controllers/v1/TestUtils.scala
@@ -1,4 +1,4 @@
-package controllers.v1
+package resource
 
 import play.api.libs.json._
 import play.api.mvc.AnyContentAsJson
@@ -6,6 +6,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.Application
 
 /**
  * TestUtils
@@ -17,7 +18,10 @@ import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
 trait TestUtils extends PlaySpec with GuiceOneAppPerSuite {
 
-  protected def requestObject(url: String, method: String = GET): FakeRequest[AnyContentAsJson] =
+  protected[this] def fakeRequest(url: String, method: String = GET, appInstance: Application = app) =
+    route(appInstance, FakeRequest(method, url)).getOrElse(sys.error(s"Route $url does not exist"))
+
+  protected def requestObject(url: String, method: String = GET) =
     FakeRequest(GET, "/").withJsonBody(Json.parse("""{ "field": "value" }"""))
 
   protected def getValue(json: Option[String]): String = json match {
@@ -26,7 +30,7 @@ trait TestUtils extends PlaySpec with GuiceOneAppPerSuite {
   }
 
   protected def getJsValue(elem: JsLookupResult): String = elem match {
-    case JsDefined(y) => y.toString
+    case JsDefined(y) => s"$y"
     case _ => sys.error("No JsValue found. Forcing test failure")
   }
 
